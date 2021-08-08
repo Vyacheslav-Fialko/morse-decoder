4038f4645dd5dcbfa15b9e395019026905ac61ad
Vyacheslav <vyacheslav1@ukr.net>
2021-08-08 15:13:17 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -38,7 +38,46 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+        let chars = [];
+    let morseChar = [];
+    let resultArr = [];
+
+    //array of encoded letters, each encoded letter's length is 10
+    for (let i = 0; i < expr.length; i = i + 10) {
+        chars.push(expr.substr(i, 10));
+    }
+
+    chars.forEach(item => {
+        if (item == '**********') resultArr.push(' ');
+        let splitted = item.split('');
+
+        //remove zeros from the beginning
+        for (let i = 0; i < splitted.length; i++) {
+            if (splitted[i] == 0) {
+                splitted.shift();
+                i = -1;
+            } else break;
+        }
+        //10 stands for dot(.), 11 stands for dash(-).
+        for (let i = 0; i < splitted.length; i = i + 2) {
+            if (splitted[i].concat(splitted[i + 1]) == '10') {
+                morseChar.push('.')
+            } else if (splitted[i].concat(splitted[i + 1]) == '11') {
+                morseChar.push('-')
+            }
+        }
+        let morseStr = morseChar.join('');
+
+        for (let key in MORSE_TABLE) {
+            if (key == morseStr) {
+                resultArr.push(MORSE_TABLE[key]);
+            }
+        }
+        morseChar = [];
+    });
+
+    let resultStr = resultArr.join('');
+    return resultStr;
 }
 
 module.exports = {
