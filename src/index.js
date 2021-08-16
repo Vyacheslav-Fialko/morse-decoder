3a930c90e74d49ed7135189d9cb6b49ea51851fe
Vyacheslav-Fialko <vyacheslav1@ukr.net>
2021-08-16 14:21:46 +0300

    feat: refact code

--- src/index.js
+++ src/index.js
@@ -1,85 +1,78 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+  ".-": "a",
+  "-...": "b",
+  "-.-.": "c",
+  "-..": "d",
+  ".": "e",
+  "..-.": "f",
+  "--.": "g",
+  "....": "h",
+  "..": "i",
+  ".---": "j",
+  "-.-": "k",
+  ".-..": "l",
+  "--": "m",
+  "-.": "n",
+  "---": "o",
+  ".--.": "p",
+  "--.-": "q",
+  ".-.": "r",
+  "...": "s",
+  "-": "t",
+  "..-": "u",
+  "...-": "v",
+  ".--": "w",
+  "-..-": "x",
+  "-.--": "y",
+  "--..": "z",
+  ".----": "1",
+  "..---": "2",
+  "...--": "3",
+  "....-": "4",
+  ".....": "5",
+  "-....": "6",
+  "--...": "7",
+  "---..": "8",
+  "----.": "9",
+  "-----": "0",
 };
 
-function decode(expr) {
-        let chars = [];
-    let morseChar = [];
-    let resultArr = [];
+const refStr = (expr) => {
+  let resArr = [];
+  for (let i = 0; i < expr.length; i += 10) {
+    let item = expr.substring(i, i + 10);
+    item === "**********"
+      ? resArr.push(" ")
+      : resArr.push(item.substring(item.indexOf("1")));
+  }
+  return resArr;
+};
 
-    //array of encoded letters, each encoded letter's length is 10
-    for (let i = 0; i < expr.length; i = i + 10) {
-        chars.push(expr.substr(i, 10));
+const morseCode = (arr) => {
+  let resArr = [];
+  arr.forEach((item) => {
+    let resStr = "";
+    for (let i = 0; i < item.length; i += 2) {
+      let str = `${item[i]}${item[i + 1]}`;
+      str === "10"
+        ? (resStr += ".")
+        : str === "11"
+        ? (resStr += "-")
+        : (resStr += item[i]);
     }
+    resArr.push(resStr);
+  });
+  return resArr;
+};
 
-    chars.forEach(item => {
-        if (item == '**********') resultArr.push(' ');
-        let splitted = item.split('');
 
-        //remove zeros from the beginning
-        for (let i = 0; i < splitted.length; i++) {
-            if (splitted[i] == 0) {
-                splitted.shift();
-                i = -1;
-            } else break;
-        }
-        //10 stands for dot(.), 11 stands for dash(-).
-        for (let i = 0; i < splitted.length; i = i + 2) {
-            if (splitted[i].concat(splitted[i + 1]) == '10') {
-                morseChar.push('.')
-            } else if (splitted[i].concat(splitted[i + 1]) == '11') {
-                morseChar.push('-')
-            }
-        }
-        let morseStr = morseChar.join('');
-
-        for (let key in MORSE_TABLE) {
-            if (key == morseStr) {
-                resultArr.push(MORSE_TABLE[key]);
-            }
-        }
-        morseChar = [];
-    });
-
-    let resultStr = resultArr.join('');
-    return resultStr;
-}
+const decode = (expr) => {
+  const chars = refStr(expr);
+  const morseChar = morseCode(chars);
+  
+  return morseChar.map(item => MORSE_TABLE[item] || item).join('');
+};
 
 module.exports = {
-    decode
-}
+  decode,
+};
